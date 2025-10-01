fdb383fab12443af5da3b4bebc0922d5ff67e3f8
SergeyZakurakin <pricezak@gmail.com>
2025-09-30 21:07:17 -0400

    Initial Commit

---
+++ SampleAnimation/SampleAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAnimationApp.swift
+//  SampleAnimation
+//
+//  Created by Sergey Zakurakin on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
